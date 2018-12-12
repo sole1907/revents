b38f4dd9572e1da5bbbceedf2e5d0b190eca8c5b
ayansola_akanmu <ayansola_akanmu@mastercard.com>
2018-12-12 13:54:51 +0300

    End of section 9

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,10 +1,12 @@
-import { combineReducers } from 'redux'
-import testReducer from '../../features/testarea/testReducer'
-import eventReducer from '../../features/event/eventReducer';
+import { combineReducers } from "redux";
+import testReducer from "../../features/testarea/testReducer";
+import eventReducer from "../../features/event/eventReducer";
+import { reducer as FormReducer } from "redux-form";
 
 const rootReducer = combineReducers({
-    test: testReducer,
-    events: eventReducer
-})
+  form: FormReducer,
+  test: testReducer,
+  events: eventReducer
+});
 
 export default rootReducer;
