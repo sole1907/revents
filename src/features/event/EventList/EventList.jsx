ed2162c6f41b01cea9c28a4205e3806448424fea
MAC <mac@MACs-MacBook-Pro.local>
2018-12-21 22:34:02 +0300

    End of section 12

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -4,6 +4,7 @@ import EventListItem from "./EventListItem";
 class EventList extends Component {
   render() {
     const { events, deleteEvent } = this.props;
+    console.log("event list >>>>", events);
     return (
       <div>
         {events.map(event => (
